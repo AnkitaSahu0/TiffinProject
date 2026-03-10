8456754d3a4d63774264a33a68b0ee319297da22
Ankita Sahu <ankitasahu849@gmail.com>
2026-03-10 17:34:02 +0530

    added root route

--- server/server.js
+++ server/server.js
@@ -33,6 +33,9 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+app.get("/", (req, res) => {
+  res.send("Tiffin Backend API Running");
+});
 
 app.use(cors());
 app.use(express.json());
